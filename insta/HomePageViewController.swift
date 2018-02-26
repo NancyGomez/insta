4c28d735c5f54323e4f3e1b42dc13d89f2a6b99e
NancyGomez <nagomez@csumb.edu>
2018-02-25 20:58:11 -0800

    Added logic to use camera or library when going into HomePage

--- insta/HomePageViewController.swift
+++ insta/HomePageViewController.swift
@@ -9,15 +9,33 @@
 import UIKit
 import Parse
 
-class HomePageViewController: UIViewController {
+class HomePageViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    // Instantiate a UIImagePickerController
+    var vc : UIImagePickerController!
+
+    
     @IBAction func onLogOut(_ sender: Any) {
         NotificationCenter.default.post(name: NSNotification.Name("didLogout"), object: nil)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        vc = UIImagePickerController()
+        vc.delegate = self
+        vc.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            print("Camera is available 📸")
+            vc.sourceType = .camera
+        } else {
+            print("Camera 🚫 available so we will use photo library instead")
+            vc.sourceType = .photoLibrary
+        }
+        // Do any additional setup after loading the view.
+        self.present(vc, animated: true, completion: nil)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,15 +43,19 @@ class HomePageViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Implement the delegate method
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : Any]) {
+        // Get the image captured by the UIImagePickerController
+        let originalImage = info[UIImagePickerControllerOriginalImage] as! UIImage
+        let editedImage = info[UIImagePickerControllerEditedImage] as! UIImage
+        // TODO: resize the image for storing in DB
+        
+        // Do something with the images (based on your use case)
+        // TODO: set image view to edited image or something like that
+        
+        // Dismiss UIImagePickerController to go back to your original view controller
+        dismiss(animated: true, completion: nil)
     }
-    */
 
 }
